b1b580eed3dc9122e809de69582985dd84167a05
Student <student@gort.(none)>
2014-05-25 14:54:25 -0400

    added sleep at the end of each edge rotation to allow for hardware-reset

--- scripts/MoveSquareNoOdometry.py
+++ scripts/MoveSquareNoOdometry.py
@@ -4,30 +4,27 @@ import logging
 from geometry_msgs.msg import Twist
 from cs1567p0.srv import *
 
-logging.basicConfig(filename="/home/student/cs1567_ws/p0.log", level=logging.DEBUG)
 def move_square():
     rospy.init_node('MoveSquareNoOdometry', anonymous=True)
     rospy.wait_for_service('constant_command')
     command = Twist()
-    e_t = 2.5
+    e_t = 4.95
     r_t = 4.15
 
     for i in range(4):
-        logging.info("Loop start.")
         try:
             send_command = rospy.ServiceProxy('constant_command', ConstantCommand)
-            command.linear.x = 0.5
+            command.linear.x = 0.25
             response = send_command(command)
             rospy.sleep(e_t) # edge 1
             command.linear.x = 0.0
             command.angular.z = -0.5
             response = send_command(command)
-            logging.debug("command.angular.z: ", command.angular.z)
             rospy.sleep(r_t) # corner 1
             command.angular.z = 0.0
             command.linear.x = 0.0
             response = send_command(command)
-            logging.info(response)
+            rospy.sleep(0.25)
 
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
